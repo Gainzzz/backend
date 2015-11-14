895a47e411f4b23e8988d522b81f0958387ce4c0
Kevin Yang <k46yang@uwaterloo.ca>
2015-11-14 15:49:42 -0500

    added muscles

--- gainzzz/muscles/views.py
+++ gainzzz/muscles/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden
+from models import *
+import json
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def index(request):
+    return HttpResponse("Hello, world. You're at the user index.")
+
+
+def all_muscles(request):
+    muscles = Muscle.objects.values('name', 'description')
+    return JsonResponse(dict(muscles=list(muscles)))
+
