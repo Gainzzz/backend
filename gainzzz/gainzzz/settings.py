f8f92a60add53414803573ab63dce835d92713ea
Kevin Yang <k46yang@uwaterloo.ca>
2015-11-14 15:42:57 -0500

    added mysql db

--- gainzzz/gainzzz/settings.py
+++ gainzzz/gainzzz/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'gainzzz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sage',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1', #52.88.98.2
+        'PORT': '3306',
     }
 }
 
@@ -87,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
